662c1347df5cdd9e5c2b41c6efad16217cd9a651
Jacob Whitlow <whitlow@princeton.edu>
2018-02-26 13:07:24 -0500

    Example

--- app/src/main/java/com/app/globerunner/globerunner/ProfileActivity.java
+++ app/src/main/java/com/app/globerunner/globerunner/ProfileActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+// Hi
 public class ProfileActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
